f65eb1c0b0598f5c129ff03111c67a04c58f8780
CaptainTurttle <38168406+CaptainTurttle@users.noreply.github.com>
2018-05-14 18:46:21 -0700

    Add files via upload

---
+++ DISCORD WEBS CODEE.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include <fstream>
+int main();
+{
+int munz;
+munz = 100;
+	if (cin >> "!munz");
+	{
+	cout << "your munz count is" munz<<endl;
+	}
+	}
